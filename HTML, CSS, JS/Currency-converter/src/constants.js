d2bbe6c37e7410bee2c6bf216cbd50d9798ee2ac
Adima Zareen <adimazareen.9975@gmail.com>
2026-08-31 01:27:46 +0530

    Create constants.js for currency codes
    Added a list of supported currency codes for conversion.

---
+++ HTML, CSS, JS/Currency-converter/src/constants.js
@@ -0,0 +1,5 @@
+// List of all supported currencies
+export const currencyCodes = [
+  "usd", "eur", "inr", "gbp", "aud", "cad", "jpy", "cny", "rub", "sgd", "nzd",
+  "hkd", "chf", "zar", "aed", "brl", "thb", "mxn", "krw", "myr"
+];
